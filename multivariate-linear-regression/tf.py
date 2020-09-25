c84630fb8c8643378bd6fc9224c112b9c4876d5a
david <davidwang@lucasgc.com>
2020-09-25 18:46:24 +0800

    mlr

--- multivariate-linear-regression/tf.py
+++ multivariate-linear-regression/tf.py
@@ -19,6 +19,53 @@ x1 = tf.constant([
 86.21
 ])
 
-x2 = tf.constant()
+x2 = tf.constant([
+3,
+2,
+2,
+3,
+1,
+2,
+3,
+2,
+2,
+3,
+1,
+1,
+1,
+1,
+2,
+2
+],dtype=tf.float32)
 
-y = tf.constant()
+y = tf.constant([
+145.00,
+110.00,
+93.00,
+116.00,
+65.32,
+104.00,
+118.00,
+91.00,
+62.00,
+133.00,
+51.00,
+45.00,
+78.50,
+69.65,
+75.69,
+95.30
+])
+
+# samples num
+n = tf.size(x1)
+
+one = tf.ones(shape=(n,))
+XT = tf.stack([one,x1,x2])
+X = tf.linalg.matrix_transpose(XT)
+Y = tf.reshape(y,shape=(n,1))
+
+# W = ((X'X)-1)X'Y
+W = tf.linalg.matmul(tf.linalg.matmul(tf.linalg.inv(tf.linalg.matmul(XT,X)),XT),Y)
+W = tf.reshape(W,[-1])
+print(W)
